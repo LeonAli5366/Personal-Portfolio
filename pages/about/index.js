7b08eb2a3d3a33518894a2b8e003245eb91809c4
LeonAli5366 <leonali274784@gmail.com>
2024-08-31 19:37:19 +0600

    hosting problem fixed

--- pages/about/index.js
+++ pages/about/index.js
@@ -26,18 +26,22 @@ const aboutData = [
       {
         title: "Web Development",
         icons: [
-          FaHtml5,
-          FaCss3,
-          FaJs,
-          FaReact,
-          SiNextdotjs,
-          SiFramer,
-          FaWordpress,
+          <FaHtml5 key={1} />,
+          <FaCss3 key={2} />,
+          <FaJs key={3} />,
+          <FaReact key={4} />,
+          <SiNextdotjs key={5} />,
+          <SiFramer key={6} />,
+          <FaWordpress key={7} />,
         ],
       },
       {
         title: "UI/UX Design",
-        icons: [FaFigma, SiAdobexd, SiAdobephotoshop],
+        icons: [
+          <FaFigma key={1} />,
+          <SiAdobexd key={2} />,
+          <SiAdobephotoshop key={3} />,
+        ],
       },
     ],
   },
@@ -123,8 +127,8 @@ const About = () => {
             exit="hidden"
             className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
           >
-            10 years ago, I began freelancing as a developer. Since then, I&apos;ve
-            done remote work for agencies, consulted for startups, and
+            10 years ago, I began freelancing as a developer. Since then,
+            I&apos;ve done remote work for agencies, consulted for startups, and
             collaborated on digital products for bussiness and consumer use
           </motion.p>
           {/* counters */}
