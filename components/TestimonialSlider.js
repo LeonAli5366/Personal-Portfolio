2a86cc51bbf06d9984a922644bc68ff323aaa45d
LeonAli5366 <leonali274784@gmail.com>
2024-09-01 16:13:44 +0600

    testimonial halfway

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,78 @@
 // testimonial data
 const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    image: "/t-avt-1.png",
+    name: "Anne Smith",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    image: "/t-avt-2.png",
+    name: "Jane Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    image: "/t-avt-3.png",
+    name: "Jhon Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
 ];
 
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// import required modules
+import { FreeMode, Pagination } from "swiper";
+
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
+// next image
+import Image from "next/image";
+
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+           <div>
+            {/* avatar, name, position */}
+            <div>
+              <div>
+                {/* avatar image */}
+                <div>avatar image</div>
+                {/* name */}
+                <div>name</div>
+                {/* position */}
+                <div>position</div>
+              </div>
+            </div>
+            {/* quote & message */}
+            <div>quote & message</div>
+           </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
